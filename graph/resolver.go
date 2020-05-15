365a453e20075a097e2c225bd70f16edc299f423
Daniel Ramirez <dxas90@gmail.com>
2020-05-15 15:40:08 +0300

    Added resolvers
    Signed-off-by: Daniel Ramirez <dxas90@gmail.com>

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,14 @@
 package graph
 
+//go:generate go run github.com/99designs/gqlgen
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
+import (
+	"github.com/dxas90/bobrgql/graph/model"
+)
 
-type Resolver struct{}
+type Resolver struct {
+	todos []*model.Todo
+}
